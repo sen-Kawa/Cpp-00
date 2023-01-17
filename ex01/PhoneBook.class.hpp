0abe5a6f2d5f8a9a08a0cbf74564dc49d2e5be9f
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-17 21:15:02 +0100

    starting phonebook class and makefile

---
+++ ex01/PhoneBook.class.hpp
@@ -0,0 +1,14 @@
+#ifndef PHONEBOOK_CLASS_HPP
+# define PHONEBOOK_CLASS_HPP
+
+#include <iostream>
+#include <string>
+
+class PhoneBook
+{
+	public:
+		PhoneBook(void);
+		~PhoneBook(void);
+};
+
+#endif
