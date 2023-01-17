84d9805303f2812de18643fe404faa382c365f34
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-17 21:24:36 +0100

    contact class

---
+++ ex01/Contact.class.cpp
@@ -0,0 +1,11 @@
+#include "Contact.class.hpp"
+
+Contact::Contact(void)
+{
+	return ;
+}
+
+Contact::~Contact(void)
+{
+	return ;
+}
---
+++ ex01/Contact.class.hpp
@@ -0,0 +1,14 @@
+#ifndef CONTACT_CLASS_HPP
+# define CONTACT_CLASS_HPP
+
+#include <iostream>
+#include <string>
+
+class Contact
+{
+	public:
+		Contact(void);
+		~Contact(void);
+};
+
+#endif
