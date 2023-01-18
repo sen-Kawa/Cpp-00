98096db7ac04cfe7986ea2ce9cd33dea5f527f84
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-18 01:32:07 +0100

    prompting for adding searching and exiting

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,9 +5,10 @@ int	main(void)
 	PhoneBook	phonebook;
 	std::string	input;
 
+	std::cout << "Welcome to your awesome phonebook" << std::endl;
 	while (input.compare("EXIT") != 0)
 	{
-		std::cout << "Hi";
+		std::cout << "Enter one of the following options:\n ADD\n SEARCH\n EXIT" << std::endl;
 		getline(std::cin, input);
 	}
 	return (0);
