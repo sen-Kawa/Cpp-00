d277bc898a0c947f8aa078572d3fb9563d4b29b2
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-18 01:56:31 +0100

    contact details attributes added

--- ex01/Contact.class.hpp
+++ ex01/Contact.class.hpp
@@ -6,6 +6,13 @@
 
 class Contact
 {
+	private:
+		std::string first_name;
+		std::string last_name;
+		std::string nickname;
+		std::string phone_number;
+		std::string darkest_secret;
+
 	public:
 		Contact(void);
 		~Contact(void);
--- ex01/PhoneBook.class.cpp
+++ ex01/PhoneBook.class.cpp
@@ -2,7 +2,9 @@
 
 int	add_contact(int contact_count)
 {
-	
+	contact[contact_count].fill_contact();	
+	contact_count++;
+	return (contact_count);
 }
 
 PhoneBook::PhoneBook(void)
