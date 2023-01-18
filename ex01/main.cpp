f56fc93e3bbf4e72087de61674ef38e602b1f67c
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-18 01:38:25 +0100

    cases for add and search

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,11 +5,19 @@ int	main(void)
 	PhoneBook	phonebook;
 	std::string	input;
 
-	std::cout << "Welcome to your awesome phonebook" << std::endl;
+	std::cout << "Welcome to your awesome phonebook!" << std::endl;
 	while (input.compare("EXIT") != 0)
 	{
-		std::cout << "Enter one of the following options:\n ADD\n SEARCH\n EXIT" << std::endl;
+		std::cout << "Enter one of the following options:\n- ADD\n- SEARCH\n- EXIT" << std::endl;
 		getline(std::cin, input);
+		if (input.compare("ADD") == 0)
+		{
+			std::cout << "Adding.." << std::endl;
+		}
+		if (input.compare("SEARCH") == 0)
+		{
+			std::cout << "Searching.." << std::endl;
+		}
 	}
 	return (0);
 }
