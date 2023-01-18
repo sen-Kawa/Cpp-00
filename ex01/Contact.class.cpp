bb7bbde96b90328ebb28c205601eda0a0dff9b2c
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-18 19:59:48 +0100

    different private functions for filling contact details

--- ex01/Contact.class.cpp
+++ ex01/Contact.class.cpp
@@ -7,7 +7,31 @@ void	fill_contact(void)
 	std::cout << "Enter your nickname" << std::endl;		
 	std::cout << "Enter your phonenumber" << std::endl;		
 	std::cout << "Enter your darkest secret" << std::endl;		
+}
 
+void	fill_first_name(void)
+{
+	std::cout << "Enter your first name" << std::endl;		
+}
+
+void	fill_last_name(void)
+{
+	std::cout << "Enter your last name" << std::endl;		
+}
+
+void	fill_nickname(void)
+{
+	std::cout << "Enter your nickname" << std::endl;		
+}
+
+void	fill_phonenumber(void)
+{
+	std::cout << "Enter your phonenumber" << std::endl;		
+}
+
+void	fill_darkest_secret(void)
+{
+	std::cout << "Enter your darkest secret" << std::endl;		
 }
 
 Contact::Contact(void)
--- ex01/Contact.class.hpp
+++ ex01/Contact.class.hpp
@@ -14,6 +14,12 @@ class Contact
 		std::string phone_number;
 		std::string darkest_secret;
 
+		void fill_first_name(void);
+		void fill_last_name(void);
+		void fill_nickname(void);
+		void fill_phone_number(void);
+		void fill_darkest_secret(void);
+
 	public:
 
 		void fill_contact(void);
