6eaa67be0efe2ef2384a7e932193e81bbf21d9b1
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-19 01:36:14 +0100

    reading input from contacts, saving it into the variables

--- ex01/Contact.class.cpp
+++ ex01/Contact.class.cpp
@@ -12,26 +12,31 @@ void	Contact::fill_contact(void)
 void	Contact::fill_first_name(void)
 {
 	std::cout << "Enter your first name" << std::endl;		
+	getline(std::cin, first_name);
 }
 
 void	Contact::fill_last_name(void)
 {
 	std::cout << "Enter your last name" << std::endl;		
+	getline(std::cin, last_name);
 }
 
 void	Contact::fill_nickname(void)
 {
 	std::cout << "Enter your nickname" << std::endl;		
+	getline(std::cin, nickname);
 }
 
 void	Contact::fill_phone_number(void)
 {
 	std::cout << "Enter your phonenumber" << std::endl;		
+	getline(std::cin, phone_number);
 }
 
 void	Contact::fill_darkest_secret(void)
 {
 	std::cout << "Enter your darkest secret" << std::endl;		
+	getline(std::cin, darkest_secret);
 }
 
 Contact::Contact(void)
